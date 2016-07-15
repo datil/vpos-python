f1e68101a2de6083253983c0829bb5915cc752fb
Eduardo Raad <eduardo.raad@datilmedia.com>
2016-07-15 00:22:55 -0500

    Adds response parsing demo and better docs.

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # vpos-python
-Use this code as a guide to implement Python clients for Datil's VPOS API.
+Use this code as a guide to implement a payment button for the Alignet VPOS Gateway using the Datil VPOS API.
 
 ## Requirements
-* Python 2.7.x
-* pip 8.1.x
+* Python 2.7.x.
+* pip 8.1.x.
+* Linux or OSX.
 
 ## Quickstart
 `pip install -r requirements.txt`
+`python client.py`
+
+Finally, visit http://localhost:5000 and click "Pay". You should be redirected to the VPOS payment page where you can enter your credit card information.
+
+When you finalize the transaction at the VPOS screen, you will be redirected to the URL you registered in the VPOS Gateway.
--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import requests
 import yaml
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from jinja2 import Environment, PackageLoader, Template
 from random import randint
 
@@ -17,16 +17,24 @@ payment_request_url = config['payment_req_url'] % {'client_id': config['client_i
 payment_response_url = config['payment_res_url'] % {'client_id': config['client_id'], 'env': config['env']}
 
 # Define headers for HTTP requests
+# An API key is always needed
 headers = {'x-api-key': config['datil_api_key']}
 
-# Define test payment data
+# Generate a random order identificator for the purpose of this demo.
+# In a production environment, your e-commerce software or ERP should
+# generate a unique order id for each payment request.
+# It must be a numeric id of length 11.
+def random_order():
+    return str(randint(10000000000,99999999999))
+
+# Define a test payment request
 test_payment = {'interest_amount': '000',
                 'ice_tax_amount': '000',
                 'iva_tax_exempt_amount': '000',
                 'language': 'SP',
                 'purchase_currency_code': '840',
                 'commerce_mall_id': '1',
-                'purchase_operation_number': str(randint(10000000000,99999999999)),
+                'purchase_operation_number': random_order(),
                 'net_amount': '1000',
                 'iva_tax_amount': '140',
                 'purchase_amount': '1140'}
@@ -40,9 +48,48 @@ def get_payment_request(req):
     r = requests.post(payment_request_url, json = req, headers = headers)
     return r.json()
 
-# Setup web server
+# Allows you to get a decrypted payment response comprised of:
+# - authorization_code
+# - tax_amount
+# - commerce_id
+# - authorization_result
+# - terminal_code
+# - cvv_cvc2_validation
+# - error_message
+# - error_code
+# - language
+# - purchase_operation_number
+# - vci
+# - interests
+# - card_expiration_month
+# - card_number
+# - purchase_ip_address
+# - purchase_currency_code
+# - acquirer_id
+# - card_expiration_year
+# - eci
+# - net_amount
+# - card_type
+# - purchase_amount
+#
+# With this data, you can build the payment "thank you" page and let
+# the payer know if the payment was successful.
+#
+# You need to look at "authorization_result". If it is "00", it means the payment
+# was processed successfully. Any other code means it was declined.
+#
+# If the transaction was successful, you can obtain the authorization code from
+# "authorization_code".
+#
+# If thee transaction was declined, you can get a detailed error from "error_message"
+# and "error_code"
+def get_payment_response(res):
+    r = requests.post(payment_response_url, json = res, headers = headers)
+    return r.json()
+
+# Visit this route to generate a payment request.
 @app.route("/")
-def vpos_client():
+def vpos_request():
     req = get_payment_request(test_payment)
     print req
     template = env.get_template('client.html')
@@ -52,6 +99,19 @@ def vpos_client():
                            ciphered_xml=req['ciphered_xml'],
                            ciphered_signature=req['ciphered_signature'],
                            ciphered_session_key=req['ciphered_session_key'])
+
+# Captures the POST parameters sent by the Alignet VPOS Gateway and sends them
+# to the Datil VPOS API for decryption.
+# Returns the response as JSON for demo purposes.
+@app.route("/response", methods=['POST'])
+def vpos_response():
+    ciphered_xml = request.form['XMLREQ']
+    ciphered_signature = request.form['DIGITALSIGN']
+    ciphered_session_key = request.form['SESSIONKEY']
+    res = get_payment_response({'ciphered_xml': ciphered_xml,
+                                'ciphered_signature': ciphered_signature,
+                                'ciphered_session_key': ciphered_session_key})
+    return jsonify(res)
 
 if __name__ == "__main__":
     app.run()
